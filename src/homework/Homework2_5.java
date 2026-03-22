6a8181f0658206a803f11e06a50c3c584441c3e2
Akhunov Gayaz <kapananas16@gmail.com>
2026-03-22 14:15:15 +0300

    added Homework

--- src/homework/Homework2_5.java
+++ src/homework/Homework2_5.java
@@ -6,11 +6,11 @@ public class Homework2_5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число");
-        int a = scanner.nextInt() - 1;//"до введенного пользователем числа", значит введенное число не должно входить в сумму?
+        int a = scanner.nextInt();//"до введенного пользователем числа", значит введенное число не должно входить в сумму?
         if (a > 0) {
             if (a % 1 == 0) {
                 int sum = 0;
-                for (int i = 1; i <= a; i++) {
+                for (int i = 1; i < a; i++) {
                     sum = sum + i;
                 }
                 System.out.println(sum);
